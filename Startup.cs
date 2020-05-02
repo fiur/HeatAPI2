0c11bc67b39b6d5419c6cac58bfa3539ed348ac9
Stefan Reinholdt Jørgensenn <stefan.jorgensen@highstage.dk>
2020-05-02 21:13:47 +0200

    Removed all .net SPA

--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,5 @@
-using JavaScriptEngineSwitcher.ChakraCore;
-using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using React.AspNet;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,9 +25,6 @@ namespace React.Sample.Webpack.CoreMvc
 		{
 			services.AddDbContext<VarmedataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("VarmeDataContext")));
 
-			services.AddJsEngineSwitcher(options => options.DefaultEngineName = ChakraCoreJsEngine.EngineName)
-				.AddChakraCore();
-
 			services.AddCors(options =>
 			{
 				options.AddPolicy(name: MyAllowSpecificOrigins,
@@ -42,27 +36,12 @@ namespace React.Sample.Webpack.CoreMvc
 
 			services.AddMvc();
 
-			services.AddReact();
-			services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-			// Build the intermediate service provider then return it
-			services.BuildServiceProvider();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostEnvironment env)
 		{
 			app.UseCors(MyAllowSpecificOrigins);
-
-			// Initialise ReactJS.NET. Must be before static files.
-			app.UseReact(config =>
-			{
-				config
-					.SetReuseJavaScriptEngines(true)
-					.SetLoadBabel(false)
-					.SetLoadReact(false)
-					.SetReactAppBuildPath("~/dist");
-			});
 
 			if (env.IsDevelopment())
 			{
@@ -76,8 +55,6 @@ namespace React.Sample.Webpack.CoreMvc
 			app.UseEndpoints(endpoints =>
 			{
 				endpoints.MapControllerRoute("default", "{path?}", new { controller = "Home", action = "Index" });
-				endpoints.MapControllerRoute("comments-root", "comments", new { controller = "Home", action = "Index" });
-				endpoints.MapControllerRoute("comments", "comments/page-{page}", new { controller = "Home", action = "Comments" });
 				endpoints.MapControllerRoute("api", "api/tude/{n}", new { controller = "get", action = "GetTude" });
 			});
 		}
